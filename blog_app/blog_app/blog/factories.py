bb750135de079d1ecedc43f51a4a9489016adb92
michalolejarczyk <michal.olejarczyk@yahoo.pl>
2020-08-16 15:17:52 +0200

    add command to create sample posts data

---
+++ blog_app/blog_app/blog/factories.py
@@ -0,0 +1,45 @@
+from factory.django import DjangoModelFactory
+from factory import Sequence, SubFactory, Faker
+from django.contrib.auth.models import User
+
+
+from django.utils import timezone
+from datetime import timedelta
+
+
+from .models import Post
+
+class UserFactory(DjangoModelFactory):
+    class Meta:
+        model = User
+        django_get_or_create = ("username",)
+
+    username = Sequence(lambda n: f"testuser{n}")
+    password = Faker(
+        "password",
+        length=10,
+        special_chars=True,
+        digits=True,
+        upper_case=True,
+        lower_case=True,
+    )
+    email = Faker("email")
+    first_name = Faker("first_name")
+    last_name = Faker("last_name")
+    is_active = True
+    is_staff = False
+
+
+class PostFactory(DjangoModelFactory):
+    class Meta:
+        model = Post
+
+    author = SubFactory(UserFactory)
+    title = Sequence(lambda n: f"Title {n}")
+    slug = Sequence(lambda n: f"title-{n}")
+    summary = Sequence(lambda n: f"Summary {n}")
+    content = Faker("text")
+    status = Post.STATUS.PUBLISH.value
+    publish_date = timezone.now() - timedelta(days=1)
+
+
---
+++ blog_app/blog_app/blog/management/commands/blog_add_sample_data.py
@@ -0,0 +1,42 @@
+import argparse
+
+from blog_app.blog.factories import PostFactory
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand, CommandError
+from django.db.utils import IntegrityError
+
+
+class Command(BaseCommand):
+    help = "Add sample posts to user."
+
+    def _check_positive(self, value):
+        int_value = int(value)
+        if int_value > 0:
+            return int_value
+        else:
+            raise argparse.ArgumentTypeexitError(
+                "%s is an invalid positive int value" % value
+            )
+
+    def add_arguments(self, parser):
+        parser.add_argument("username", type=str)
+        parser.add_argument(
+            "quantity", type=self._check_positive, help="number of posts"
+        )
+
+    def handle(self, *args, **options):
+        username, quantity = options["username"], options["quantity"]
+        try:
+            user = User.objects.get(username=username)
+            for i in range(quantity):
+                PostFactory.create(author=user)
+        except User.DoesNotExist:
+            raise CommandError("User with username '%s' does not exist" % username)
+        except IntegrityError as e:
+            raise CommandError("Articles already exists: %s" % e)
+
+        self.stdout.write(
+            self.style.SUCCESS(
+                "Successfully created %s articles for '%s' " % (quantity, username)
+            )
+        )
